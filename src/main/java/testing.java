5fbbfaf0143f4f9ba1629c8d16d682f9adf96db8
ThienPham-Si <82398747+ThienPham-Si@users.noreply.github.com>
2022-11-27 02:13:59 -0600

    fixed dependencies

--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -49,39 +49,8 @@ public class testing {
         try {
             ExprEvaluator util = new ExprEvaluator();
 
-            // Convert an expression to the internal Java form:
-            // Note: single character identifiers are case sensitive
-            // (the "D()" function identifier must be written as upper case
-            // character)
-            String javaForm = util.toJavaForm("D(sin(x)*cos(x),x)");
-            // prints: D(Times(Sin(x),Cos(x)),x)
-            System.out.println("Out[1]: " + javaForm.toString());
-
-            // Use the Java form to create an expression with F.* static
-            // methods:
-            ISymbol x = F.Dummy("x");
-            IAST function = F.D(F.Times(F.Sin(x), F.Cos(x)), x);
-            IExpr result = util.eval(function);
-            // print: Cos(x)^2-Sin(x)^2
-            System.out.println("Out[2]: " + result.toString());
-
-            // Note "diff" is an alias for the "D" function
-            result = util.eval("diff(sin(x)*cos(x),x)");
-            // print: Cos(x)^2-Sin(x)^2
-            System.out.println("Out[3]: " + result.toString());
-
-            // evaluate the last result (% contains "last answer")
-            result = util.eval("%+cos(x)^2");
-            // print: 2*Cos(x)^2-Sin(x)^2
-            System.out.println("Out[4]: " + result.toString());
-
-            // evaluate an Integrate[] expression
-            result = util.eval("integrate(sin(x)^5,x)");
-            // print: 2/3*Cos(x)^3-1/5*Cos(x)^5-Cos(x)
-            System.out.println("Out[5]: " + result.toString());
-
             // set the value of a variable "a" to 10
-            result = util.eval("a=10");
+            IExpr result = util.eval("a=10");
             // print: 10
             System.out.println("Out[6]: " + result.toString());
 
@@ -105,12 +74,6 @@ public class testing {
             result = util.eval("fac(10)");
             // print: 3628800
             System.out.println("Out[9]: " + result.toString());
-
-            function = F.Function(F.Divide(F.Gamma(F.Plus(F.C1, F.Slot1)), F.Gamma(F.Plus(F.C1, F.Slot2))));
-            // eval function ( Gamma(1+#1)/Gamma(1+#2) ) & [23,20]
-            result = util.evalFunction(function, "23", "20");
-            // print: 10626
-            System.out.println("Out[10]: " + result.toString());
         } catch (SyntaxError e) {
             // catch Symja parser errors here
             System.out.println(e.getMessage());
@@ -124,10 +87,6 @@ public class testing {
         } catch (final OutOfMemoryError oome) {
             System.out.println(oome.getMessage());
         }
-
-
-
-
 
     }
 }
